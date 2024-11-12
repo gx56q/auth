6888149bcb85f6371cd5b812ae007dac4006cc3e
voinov <voinov@skbkontur.ru>
2024-11-12 15:29:48 +0500

    task6

--- PhotosApp/Areas/Identity/IdentityHostingStartup.cs
+++ PhotosApp/Areas/Identity/IdentityHostingStartup.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -8,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using PhotosApp.Areas.Identity;
 using PhotosApp.Areas.Identity.Data;
 using PhotosApp.Services;
@@ -89,9 +92,37 @@ namespace PhotosApp.Areas.Identity
                         options.ClientSecret = context.Configuration["Authentication:Google:ClientSecret"];
                     });
 
+                services.AddAuthentication()
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = false,
+                            ValidateAudience = false,
+                            ValidateLifetime = true,
+                            ClockSkew = TimeSpan.Zero,
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = TemporaryTokens.SigningKey
+                        };
+                        options.Events = new JwtBearerEvents
+                        {
+                            OnMessageReceived = c =>
+                            {
+                                c.Token = c.Request.Cookies[TemporaryTokens.CookieName];
+                                return Task.CompletedTask;
+                            }
+                        };
+                    });
+
                 services.AddScoped<IAuthorizationHandler, MustOwnPhotoHandler>();
                 services.AddAuthorization(options =>
                 {
+                    options.DefaultPolicy = new AuthorizationPolicyBuilder(
+                        JwtBearerDefaults.AuthenticationScheme,
+                        IdentityConstants.ApplicationScheme)
+                        .RequireAuthenticatedUser()
+                        .Build();
                     options.AddPolicy(
                         "Beta",
                         policyBuilder =>
@@ -112,6 +143,16 @@ namespace PhotosApp.Areas.Identity
                         {
                             policyBuilder.RequireAuthenticatedUser();
                             policyBuilder.AddRequirements(new MustOwnPhotoRequirement());
+                        });
+                    options.AddPolicy(
+                        "Dev",
+                        policyBuilder =>
+                        {
+                            policyBuilder.RequireAuthenticatedUser();
+                            policyBuilder.RequireRole("Dev");
+                            policyBuilder.AddAuthenticationSchemes(
+                                JwtBearerDefaults.AuthenticationScheme,
+                                IdentityConstants.ApplicationScheme);
                         });
                 });
             });
